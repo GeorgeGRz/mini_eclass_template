aa55f3c08f4ce8211b4916b01767f43a42e6b716
GeorgeGRz <moustak85@hotmail.com>
2022-06-09 13:10:39 +0300

    Added livechat tooltips displaying user ids

--- mysite/classes/templatetags/check_if_enrolled.py
+++ mysite/classes/templatetags/check_if_enrolled.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='enrolled')
 def enrolled(user, course):
     return course.enrolledPeople.filter(pk=user.id).exists()
+
+@register.filter(name='getUserById')
+def getUserByID(user,userid):
+    print(userid)
+    return User.objects.get(pk=userid)
